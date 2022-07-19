187717a257d75c8601b5fc62398bb1ade24ccc2c
ogbonnachinecherem <sundayconfidence657@gmail.com>
2022-07-19 00:52:39 +0000

    react calculator post version1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Body from './components/Body';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Body/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+function Body() {
+    return (
+        <>
+        <div className="container">
+		<div className="row rw">
+			<div className="col-md-12">0</div>
+			<div className="col-md-3">AC</div>
+			<div className="col-md-3">+/-</div>
+			<div className="col-md-3">%</div>
+
+			<div className="col-md-3 color">&#247;</div>
+			<div className="col-md-3">7</div>
+			<div className="col-md-3">8</div>
+			<div className="col-md-3">9</div>
+			<div className="col-md-3 color">x</div>
+			<div className="col-md-3">4</div>
+
+			<div className="col-md-3">5</div>
+			<div className="col-md-3">6</div>
+			<div className="col-md-3 color">-</div>
+			<div className="col-md-3">1</div>
+			<div className="col-md-3">2</div>
+			<div className="col-md-3">3</div>
+			<div className="col-md-3 color">+</div>
+			<div className="col-md-6">0</div>
+			<div className="col-md-3">.</div>
+			<div className="col-md-3 color eq">=</div>
+			
+		</div>	
+	</div>
+
+        </>
+    );
+}
+export default Body;
